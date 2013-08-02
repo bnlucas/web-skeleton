14b3d69271c9b67f2010a75434ee73b30c3bc45d
Nathan Lucas <nathan@bnlucas.com>
2013-08-01 23:44:22 -0600

    xhtml push
    XHTML web skeleton with Compass support.

---
+++ xhtml/contrib.rb
@@ -0,0 +1,9 @@
+css_dir = 'res/css'
+sass_dir = 'com/scss'
+http_path = '/'
+images_dir = 'res/images'
+javascripts_dir = 'com/js'
+
+output_style = :compact # :expanded, :nested, :compact, :compressed
+
+line_comments = false # true, false
