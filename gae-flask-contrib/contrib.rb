1024a2a23a01b458f87a326be94b2cbb529058a2
Nathan Lucas <bnlucas@outloo.com>
2015-06-03 01:23:46 -0600

    GAE with Flask and Contrib config.

---
+++ gae-flask-contrib/contrib.rb
@@ -0,0 +1,9 @@
+css_dir = 'application/static/css'
+sass_dir = 'application/static/scss'
+http_path = '/'
+images_dir = 'application/static/img'
+javascripts_dir = 'application/static/js'
+
+output_style = :compact # :expanded, :nested, :compact, :compressed
+
+line_comments = false # true, false
